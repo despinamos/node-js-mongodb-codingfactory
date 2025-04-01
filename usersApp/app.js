bb7448f6aefc5198764ddfbbc64e7711c3076de7
D. Moschokarfi <despinamx16@gmail.com>
2025-04-01 14:32:56 +0300

    feat: user route and controller user product route and controller

--- usersApp/app.js
+++ usersApp/app.js
@@ -1,10 +1,12 @@
-const express = require("express")
+const express = require("express");
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
 
-const user = require('./routes/user.routes')
+const user = require('./routes/user.routes');
+const userProduct = require('./routes/user.products.routes')
 
-app.use('/api/users', user)
+app.use('/api/users', user);
+app.use('/api/user-product', userProduct);
 
 module.exports = app
