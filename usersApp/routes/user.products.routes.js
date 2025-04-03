6dd0b561550276101f338f4bbfa50246c6dd0585
D. Moschokarfi <despinamx16@gmail.com>
2025-04-03 11:51:13 +0300

    feat: jwt and swagger updates

--- usersApp/routes/user.products.routes.js
+++ usersApp/routes/user.products.routes.js
@@ -5,6 +5,9 @@ const userProductController = require('../controllers/user.product.controller');
 
 router.get('/', userProductController.findAll);
 router.get('/:username', userProductController.findOne);
-router.post('/', userProductController.create); 
+router.post('/', userProductController.create);
+router.patch('/:username', userProductController.update);
+router.delete('/:username/products/:_id', userProductController.delete);
+router.get('/stats/stats1', userProductController.stats1);
 
 module.exports = router;
